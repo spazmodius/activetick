a8c819b1cd832f5d61b2a454b5f451a6b049bed7
rajiv <rajiv.delwadia@versionone.com>
2015-05-21 23:24:27 -0400

    initialize and shutdown the ActiveTick api

--- ActiveTickServerAPI.node.cpp
+++ ActiveTickServerAPI.node.cpp
@@ -1,14 +1,29 @@
 #include <node.h>
+#include <ActiveTickServerAPI.h>
 
 using namespace v8;
+
+namespace ActiveTickServerAPI_node {
 
 Handle<Value> Method(const Arguments& args) {
 	HandleScope scope;
 	return scope.Close(String::New("world"));
 }
 
-void init(Handle<Object> target) {
+void onExit(void*);
+
+void onInit(Handle<Object> target) {
+	ATInitAPI();
+	node::AtExit(onExit);
+
 	target->Set(String::NewSymbol("hello"),
 		FunctionTemplate::New(Method)->GetFunction());
 }
-NODE_MODULE(ActiveTickServerAPI, init)
+
+void onExit(void*) {
+	ATShutdownAPI();
+}
+
+NODE_MODULE(ActiveTickServerAPI, onInit)
+
+}
